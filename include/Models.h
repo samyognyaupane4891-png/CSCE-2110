74055297bedc5df71a107ba974c5c110e12e78de
Samyog Nyaupane <samyog.nyaupane4891@gmail.com>
2026-09-10 14:33:27 -0500

    Initial project skeleton, CMake setup, and Models header

---
+++ include/Models.h
@@ -0,0 +1,29 @@
+#ifndef MODELS_H
+#define MODELS_H
+
+#include <string>
+#include <iostream>
+
+
+struct Resource {
+    std::string id;
+    std::string name;
+    std::string type;
+    std::string status; // "Available" or "Unavailable"
+};
+
+struct Reservation {
+    int reservationId;
+    int studentId;
+    std::string studentName;
+    std::string resourceId;
+    std::string date;
+};
+
+struct StudentRequest {
+    int studentId;
+    std::string studentName;
+    std::string resourceId;
+};
+
+#endif
